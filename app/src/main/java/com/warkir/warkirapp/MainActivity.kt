ac0ed946b9cdc5111988a4b4ebc349de2a6ab913
yaumil <yaumilramkhadani@gmail.com>
2025-11-22 17:22:55 +0700

    fix error

--- app/src/main/java/com/warkir/warkirapp/MainActivity.kt
+++ app/src/main/java/com/warkir/warkirapp/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         viewCompat()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.main)
-        alsdmlasldmkasmkdmakd
     }
 
     /*
